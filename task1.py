c9ce1fb6ec1c9dc48aef920fb24e2f734e42bd09
rajatnath17 <rajatnath17@gmail.com>
2025-09-25 01:08:19 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,12 @@
+
+git config --global user.name "rajatnath17"
+
+a=input('Insert the first number: ')
+b=input('Insert the second number: ')
+a=int(a)
+b=int(b)
+print('addition:',(a+b))
+print('subtraction:',(a-b))
+print('multiplication:',(a*b))
+print('division:',(a/b))
+
